ebb35f78c31366a64024a3b0a6dcc3d48f1c06db
Greg Miller <hypurban86@gmail.com>
2025-05-28 12:58:14 -0400

    Implements ThreeQuery - works!

--- src/components/ThreeDemo.vue
+++ src/components/ThreeDemo.vue
@@ -6,7 +6,10 @@
 -->
 <template>
 
-	<div class="three-demo">
+	<div 
+		ref="containerEl"
+		class="three-demo"
+	>
 		<h1>Three.js Demo</h1>
 		<p>This is a simple Three.js scene to demonstrate the library.</p>
 	</div>
@@ -15,6 +18,46 @@
 
 // vue
 import { ref, onMounted } from 'vue'
+
+// dom elements
+const containerEl = ref(null);
+
+// threeJS stuffs
+import ThreeQuery from 'three-query';
+
+// stuffs from our library
+let $ = null;
+let details = {};
+
+// set up ThreeJS on mounted
+onMounted(()=>{
+
+	// build default scene from our Lib!
+	const container = containerEl.value
+	const { scene, lights, cube, camera, renderer } = ThreeQuery.createScene(container, {
+		autoSize: true,
+		autoRender: true,
+		addCube: true,
+		addLights: true,
+		addControls: false,
+	});
+
+	// disable the background
+	renderer.setClearColor(0x000000, 0);
+
+	// save to our globals
+	$ = new ThreeQuery(scene);
+	details = {
+		scene,
+		lights,
+		cube,
+		container,
+		camera
+	};
+
+	window.details = details;
+
+});
 
 </script>
 <style lang="scss" scoped>
